68ec47e08fda00355d35c40abae25f3a975a90e3
Lex Peterson <lpeterson@chorus.net>
2021-04-06 13:58:45 -0700

    initial commit, setups and installs

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+Rack::MiniProfiler.config.position = 'bottom-right'
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
